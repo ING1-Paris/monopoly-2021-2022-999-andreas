fcf28276599f519997dd69b4773d52e4b47cd2fd
999-andreas <59205956+999-andreas@users.noreply.github.com>
2021-12-11 15:21:07 +0100

    test

---
+++ LE_Monopoly/menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "sous-programmes.c"
+
+
+int nb_alea();
+int lancer_de();
+void banque();
+
+#endif // MENU_H_INCLUDED
