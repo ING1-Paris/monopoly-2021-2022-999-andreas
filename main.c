b5dc2d4073d44a5298119e3c0bbc6db4e352de1a
Maxdelprat <95747937+Maxdelprat@users.noreply.github.com>
2021-12-19 17:03:35 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
+
 #include "menu.h"
 
 int main()
 {
     srand(time(NULL));
-
-
-    lancer_de();
-
-    banque();
-
-
-
-
+    t_mono plat[4][8];
+    int de[3];
+    char pion;
+    int ligne=2;
+    int colplat,ligplat,a;
+    int joueur=0;
+    setConsoleSize();
+    int choix;
+    choix=menu();
+    fflush(stdin);
+    printf("entrez un caractère");
+    scanf("%c",&pion);
+    if (choix==1)
+    {
+        for(int j=0;j<7;j++){
+        gotoligcol(31+ligne,70);
+        printf("On relance les des");
+        scanf("%d",&a);
+        if (a==1){
+        system('cls');
+        plateau(ligne);
+        gotoligcol(29+ligne,70);
+        printf("vous avez fait:");
+        gotoligcol(30+ligne,75);
+        lancer_de(de);
+        joueur=(joueur+de[0]+de[1])%32;
+        afficher_point(joueur,ligplat,colplat,pion,plat,ligne);
+        }
+    }
+    }
+    gotoligcol(60+ligne,1);
     return 0;
 }
-
--- menu.h
+++ menu.h
@@ -1,15 +1,27 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
-#include "sous-programmes.c"
+typedef struct monopoly
+{
+    int maison;
+    int hotel;
+    int presence;
+    int prix;
+
+}t_mono;
 
 
 int nb_alea();
-int lancer_de();
-void banque();
+void lancer_de(int de[3]);
+int menu();
+void Color(int couleurDuTexte,int couleurDeFond);
+void gotoligcol( int lig, int col );
+void setConsoleSize();
+void plateau(ligne);
+void afficher_point(int joueur,int ligplat, int colplat,char pion,t_mono plat[4][8],int ligne);
 
 #endif // MENU_H_INCLUDED
